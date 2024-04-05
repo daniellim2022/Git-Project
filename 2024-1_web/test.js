c0aaa6a8af2da4f6a328f898903d01e712dd17d4
daniellim2022 <chdaniellim@gmail.com>
2024-04-05 09:35:50 +0900

    2024/4/5-W04

---
+++ 2024-1_web/test.js
@@ -0,0 +1,6 @@
+function over_out(obj) {
+    obj.src="https://cdn.imweb.me/thumbnail/20190610/89b646bc2b04f.png";
+}
+function out_out(obj) {
+    obj.src="https://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2016/08/04/20160804000620_0.jpg";
+}
